126a2171fa05a4629975f65fb78cb1583a563b0f
Oscar Andres Ramos Lopez <oscar1940_1@hotmail.com>
2025-01-10 21:49:12 -0500

    feat: crear proyecto de pruebas -Se implementa ConcatenateStringsTest

---
+++ StringManipulationTests/StringOperationsTest.cs
@@ -0,0 +1,16 @@
+﻿using StringManipulation;
+
+namespace StringManipulationTests
+{
+    public class StringOperationsTest
+    {
+        [Fact]
+        public void ConcatenateStringsTest()
+        {
+            var operations =  new StringOperations();
+            var result = operations.ConcatenateStrings("Cadena1", "Cadena2");
+            Assert.Equal("Cadena1 Cadena2", result); 
+
+        }
+    }
+}
---
+++ StringManipulationTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace StringManipulationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
